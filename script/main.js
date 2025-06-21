02f5cbd09633ecd3f1040386591ebb449ee4874f
unknown <khanhdjkl@gmail.com>
2025-06-21 18:20:45 +0700

    ud2

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Chị nghe một chút nhạc nhé ?',
+        title: 'Bạn nghe một chút nhạc nhé ?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
